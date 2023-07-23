df67b5df0951bfc8279629cb350fdc78b35f62e2
Paapa Quansah <87806813+iampaapa@users.noreply.github.com>
2023-07-23 16:03:38 +0000

    Update __init__.py

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -2,8 +2,8 @@ import os
 
 from flask import Flask 
 
-from extensions import db
-from routes import main
+from .extensions import db
+from .routes import main
 
 def create_app():
     app = Flask(__name__)
