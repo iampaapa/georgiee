3a18fc2d5f43793286a2ac907cc719190a988c73
Paapa Quansah <87806813+iampaapa@users.noreply.github.com>
2023-07-23 16:10:28 +0000

    Update __init__.py

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -8,7 +8,7 @@ from .routes import main
 def create_app():
     app = Flask(__name__)
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://georgette_project_user:xOLCvgOroVw5S4C3BBFbdTdasca4Qts6@dpg-ciuio7liuiedpv0b2prg-a.oregon-postgres.render.com/georgette_project"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
     # postgres://georgette_project_user:xOLCvgOroVw5S4C3BBFbdTdasca4Qts6@dpg-ciuio7liuiedpv0b2prg-a.oregon-postgres.render.com/georgette_project
 
     db.init_app(app)
