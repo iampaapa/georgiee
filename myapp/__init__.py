2a07ad326c02015da3b3d6a8c277e0603b9c5184
Paapa Quansah <87806813+iampaapa@users.noreply.github.com>
2023-07-24 20:10:11 +0000

    Update __init__.py

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -9,8 +9,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
-    # postgres://georgette_project_user:xOLCvgOroVw5S4C3BBFbdTdasca4Qts6@dpg-ciuio7liuiedpv0b2prg-a.oregon-postgres.render.com/georgette_project
-
+    
     db.init_app(app)
 
     app.register_blueprint(main)
